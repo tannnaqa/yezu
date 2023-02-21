389b50ee78c94dc9cc29e3aaec4970e7b87e796f
tannnaqa <tannnaqa@gmail.com>
2023-02-21 23:23:28 +0100

    Implement the 'Parser' class

--- yezu.py
+++ yezu.py
@@ -2,13 +2,16 @@ import enum
 import re
 import sys
 
+def report(message):
+    print(message, file=sys.stderr)
+    exit()
+
 def readfile(path):
     try:
         with open(path, "r", encoding="utf8") as file:
             return file.read()
     except FileNotFoundError:
-        print(f"ERROR: file {path!r} not found", file=sys.stderr)
-        exit()
+        report(f"ERROR: file {path!r} not found")
 
 class Position:
     __slots__ = "path", "line", "row"
@@ -78,8 +81,17 @@ class Scanner:
             return pos, self.string[start:self.index]
 
 class Kind(enum.Enum):
+    def __repr__(self):
+        return self.name
+    
     def __str__(self):
         return self.name
+    
+    def filter(self, iterable):
+        return filter(lambda token: token.kind == self, iterable)
+    
+    def exclude(self, iterable):
+        return filter(lambda token: token.kind != self, iterable)
     
     ILLEGAL = enum.auto()
     COMMENT = enum.auto()
@@ -95,6 +107,7 @@ class Kind(enum.Enum):
     END = enum.auto()
     IF = enum.auto()
     THEN = enum.auto()
+    ELIF = enum.auto()
     ELSE = enum.auto()
     WHILE = enum.auto()
     DO = enum.auto()
@@ -119,6 +132,9 @@ class Token:
     
     def __repr__(self):
         return f"Token({self.pos}, {self.kind}, {self.value!r})"
+    
+    def __iter__(self):
+        return iter((self.pos, self.kind, self.value))
 
 REGEX_INTEGER = re.compile(r"^-?\d+$")
 REGEX_FLOAT = re.compile(r"^-?\d+\.\d+$")
@@ -155,18 +171,182 @@ class Lexer:
     
     def __next__(self):
         return tokenize(*next(self.scanner))
+    
+    def list(self):
+        return list(self)
+
+VALUE_KINDS = {
+    Kind.INTEGER,
+    Kind.FLOAT,
+    Kind.BOOLEAN,
+    Kind.STRING,
+    Kind.CHARACTER,
+}
+
+class Program:
+    __slots__ = "functions", "body"
+    
+    def __init__(self, functions, body):
+        self.functions = functions
+        self.body = body
+    
+    def __repr__(self):
+        return f"Program({len(self.functions)} functions)"
+    
+
+class Function:
+    __slots__ = "pos", "name", "body"
+    
+    def __init__(self, pos, name, body):
+        self.pos = pos
+        self.name = name
+        self.body = body
+    
+    def __repr__(self):
+        return f"Function({self.name} @{self.pos})"
+
+class If:
+    __slots__ = "pos", "cases"
+    
+    def __init__(self, pos, cases):
+        self.pos = pos
+        self.cases = cases
+    
+    def __repr__(self):
+        return f"If(@{self.pos})"
+
+class While:
+    __slots__ = "pos", "cond", "body"
+    
+    def __init__(self, pos, cond, body):
+        self.pos = pos
+        self.cond = cond
+        self.body = body
+    
+    def __repr__(self):
+        return f"While(@{self.pos})"
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = iter(tokens)
+        self.current = None
+        self.next_token()
+    
+    def next_token(self):
+        try:
+            self.current = next(self.tokens)
+        except StopIteration:
+            self.current = None
+        else:
+            return self.current
+    
+    def current_and_update(self):
+        temp = self.current
+        self.next_token()
+        return temp
+    
+    def match(self, *kinds):
+        return self.current is not None and self.current.kind in kinds
+    
+    def expect(self, *kinds):
+        if self.current is None:
+            report(f"ERROR: expected {kinds} but found nothing")
+        elif self.current.kind not in kinds:
+            report(f"ERROR: expected {kinds} but found {self.current.kind} at {self.current.pos}")
+        else:
+            return self.current_and_update()
+    
+    def collect(self, *stops):
+        collected = []
+        while self.current is not None and self.current.kind not in stops:
+            collected.append(self.parse_atom())
+        return collected
+    
+    def __iter__(self):
+        return self
+    
+    @property
+    def program(self):
+        prog = Program([], [])
+        for atom in self:
+            if isinstance(atom, Function):
+                prog.functions.append(atom)
+            else:
+                prog.body.append(atom)
+        return prog
+    
+    def __next__(self):
+        if self.current is None:
+            raise StopIteration()
+        elif self.current.kind == Kind.FUN:
+            return self.parse_function()
+        else:
+            return self.parse_atom()
+    
+    def parse_function(self):
+        pos, _, _ = self.expect(Kind.FUN)
+        _, _, name = self.expect(Kind.WORD)
+        body = self.collect(Kind.END)
+        self.expect(Kind.END)
+        return Function(pos, name, body)
+    
+    def parse_atom(self):
+        if self.current.kind in VALUE_KINDS:
+            return self.current_and_update()
+        elif self.current.kind == Kind.WORD:
+            return self.current_and_update()
+        elif self.current.kind == Kind.IF:
+            return self.parse_if()
+        elif self.current.kind == Kind.WHILE:
+            return self.parse_while()
+        else:
+            report(f"ERROR: unexpected {self.current.kind} token at {self.current.pos}")
+    
+    def parse_if(self):
+        cases = []
+        pos, _, _ = self.expect(Kind.IF)
+        cond = self.collect(Kind.THEN)
+        self.expect(Kind.THEN)
+        body = self.collect(Kind.ELIF, Kind.ELSE, Kind.END)
+        cases.append((cond, body))
+        while self.match(Kind.ELIF):
+            self.expect(Kind.ELIF)
+            cond = self.collect(Kind.THEN)
+            self.expect(Kind.THEN)
+            body = self.collect(Kind.ELIF, Kind.ELSE)
+            cases.append((cond, body))
+        if self.match(Kind.ELSE):
+            self.expect(Kind.ELSE)
+            body = self.collect(Kind.END)
+            cases.append((None, body))
+        self.expect(Kind.END)
+        return If(pos, cases)
+    
+    def parse_while(self):
+        pos, _, _ = self.expect(Kind.WHILE)
+        cond = self.collect(Kind.DO)
+        self.expect(Kind.DO)
+        body = self.collect(Kind.END)
+        self.expect(Kind.END)
+        return While(pos, cond, body)
 
 def main():
     print("[yezu]")
     
     if len(sys.argv) != 2:
-        print(f"USAGE: {sys.argv[0]} <file>", file=sys.stderr)
-        exit()
+        report(f"USAGE: {sys.argv[0]} <file>")
     else:
         path = sys.argv[1]
-        for token in Lexer(path):
-            print(token)
-
+        tokens = Lexer(path).list()
+        
+        illegals = list(Kind.ILLEGAL.filter(tokens))
+        if illegals:
+            report(f"ERROR: illegal word(s):\n" + "\n".join(
+                f"    {token.value!r} at {token.pos}" for token in illegals))
+        
+        program = Parser(Kind.COMMENT.exclude(tokens)).program
+        
+        print(program)
 
 if __name__ == "__main__":
     main()
